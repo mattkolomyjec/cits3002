022dfe61627c6a7ca6f26c1e345ddfd641c88a2b
Matthew Kolomyjec <matt@remediumlaw.com>
2020-04-25 12:48:37 +0800

    inital

---
+++ Route.java
@@ -0,0 +1,33 @@
+public class Route {
+
+    /**
+     * A method to "ping" a given node and return an array with all the destinations
+     * it can travel too directly
+     * 
+     * @param macAddress
+     * @return desintations[]
+     */
+    public String[] nextNodeDestinations(int macAddress) {
+
+    }
+
+    /**
+     * A method to "walk" the nodes until the destination is found, storing the path
+     * on the way.
+     * 
+     * @param origin
+     * @param destination
+     * @return restult[] an array with the path taken to reach the destination
+     */
+    public String[] routeSearch(String origin, String destination) {
+
+    }
+
+    public static void main(String args[]) {
+        String origin = args[1];
+        String destination = args[2];
+
+        // run the routeSearch method here
+
+    }
+}
